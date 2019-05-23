1b8dc9752508d8ff391e3760c0c0ec383c7b4c1c
和也山本 <kazuya2137@gmail.com>
2019-05-23 13:06:26 +0800

    first commit

---
+++ 課題１/js/app.js
@@ -0,0 +1,13 @@
+//jQueryのライブラリの読み込み
+
+$(function() {
+　$('.Toggle').click(function() {
+　　$(this).toggleClass('active');
+
+　if ($(this).hasClass('active')) {
+　　$('.NavMenu').addClass('active');　 //クラスを付与
+　} else {
+　　$('.NavMenu').removeClass('active'); //クラスを外す
+　}
+　});
+});
